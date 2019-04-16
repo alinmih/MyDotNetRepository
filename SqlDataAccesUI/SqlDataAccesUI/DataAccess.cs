b7d568208d4d67d524fca25899c982b686041227
Alin Mihailescu <alin.d.mihailescu@gmail.com>
2019-04-16 17:39:32 +0300

    Data Acces using Dapper
    App for testing SQL Data access

---
+++ SqlDataAccesUI/SqlDataAccesUI/DataAccess.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace SqlDataAccesUI
+{
+    public class DataAccess
+    {
+        public List<Person> GetPeople(string name)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("OrdersDB")))
+            {
+                //var output = connection.Query<Person>($"Select * from People where Name='{ name }'").ToList();
+                var output = connection.Query<Person>("dbo.People_GetByName @Name", new { Name = name }).ToList();
+                return output;
+            }
+        }
+
+        public void InsertPerson(string name, string location, string phone)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("OrdersDB")))
+            {
+                Person newPerson = new Person { Name= name, Location= location, Phone=phone};
+                List<Person> people = new List<Person>();
+                people.Add(newPerson);
+                connection.Execute("dbo.People_Insert @Name, @Location, @Phone", people);
+            }
+        }
+    }
+}
